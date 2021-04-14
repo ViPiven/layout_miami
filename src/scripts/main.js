c562228a2176a3b0b2274ce6c46a8c861e735c0c
ViPiven <vitalijjpiven777@gmail.com>
2021-04-14 14:54:09 +0300

    make build

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-document.querySelector('.header__burger').addEventListener('click', () => {
-  document.querySelector('.header__burger-icon').classList.toggle('active');
-  document.querySelector('.header__top').classList.toggle('active');
-  document.querySelector('.header__nav').classList.toggle('animate');
-});
